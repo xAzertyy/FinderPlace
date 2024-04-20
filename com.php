93065ff761c5ffa8022093180b6f60db5e367815
Antonino62 <96483829+Antonino62@users.noreply.github.com>
2024-04-20 04:10:10 +0200

    Update com.php
    Added Font Awesome requirements

--- com.php
+++ com.php
@@ -9,6 +9,8 @@ include_once "navbar.php";
 <link rel="stylesheet" type="text/css" href="./style.css">
 <script type="module" src="./index.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://use.fontawesome.com/releases/v6.2.0/js/all.js"></script>
+
 <style>
     #nav {
 
