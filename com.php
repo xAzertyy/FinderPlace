4b632c74d619b8dd79a8c7e0807941f065f6f5c8
Antonino62 <96483829+Antonino62@users.noreply.github.com>
2024-04-22 23:59:51 +0200

    Update com.php
    Removed unecessary dependencies

--- com.php
+++ com.php
@@ -7,7 +7,6 @@ include_once "navbar.php";
 
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <link rel="stylesheet" type="text/css" href="./style.css">
-<script type="module" src="./index.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://use.fontawesome.com/releases/v6.2.0/js/all.js"></script>
 
