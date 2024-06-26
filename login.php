f56ab2d101aaf4d28870656b9557584f9e7510f4
Antonino62 <96483829+Antonino62@users.noreply.github.com>
2024-06-27 00:26:16 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,10 @@
 
 <head>
     <title>Accesso</title>
-
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Finder Place</title>
+        <link rel="stylesheet" href="style.css">
 </head>
 
 <body id="log">
@@ -19,10 +22,7 @@
 
         
 
-    }
-
- 
-    
+    }    
 
     if (!isset($_SESSION['$password'])) { ?>
 
